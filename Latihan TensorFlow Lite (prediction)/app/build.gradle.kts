41644738249572310816dc4af33045dc7b6d2e50
haikal <asmanhaikal@gmail.com>
2024-11-06 14:37:55 +0700

    🆕 feat(tflite-prediction): completing 'Prediction using Regression with Interpreter exercise'

--- Latihan TensorFlow Lite (prediction)/app/build.gradle.kts
+++ Latihan TensorFlow Lite (prediction)/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.siaptekno.latihantensorflowliteprediction"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.siaptekno.latihantensorflowliteprediction"
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.gms:play-services-tflite-java:16.1.0")
+    implementation("com.google.android.gms:play-services-tflite-gpu:16.2.0")
+
 }
